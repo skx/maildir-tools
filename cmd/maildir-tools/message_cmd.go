84a034bd826ff57564619e6b299c1a8fe97b78cc
Steve Kemp <steve@steve.org.uk>
2020-01-15 20:39:40 +0200

    Updated to allow format-file to be specified.
    And to aid discovery and usage we can dump the default template.

--- cmd/maildir-tools/message_cmd.go
+++ cmd/maildir-tools/message_cmd.go
@@ -7,13 +7,31 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"text/template"
 
 	"github.com/google/subcommands"
 	"github.com/skx/maildir-tools/mailreader"
 )
 
+var (
+	defaultTemplate = `To: {{.To}}
+From: {{.From}}
+Cc: {{.Cc}}
+Date: {{.Date}}
+Subject: {{.Subject}}
+
+{{.Body}}`
+)
+
+// messageCmd holds our state
 type messageCmd struct {
+
+	// Template points to a template to use for rendering the mail.
+	template string
+
+	// If this flag is true we just dump our template
+	dumpTemplate bool
 }
 
 //
@@ -23,7 +41,9 @@ func (*messageCmd) Name() string     { return "message" }
 func (*messageCmd) Synopsis() string { return "Show a message." }
 func (*messageCmd) Usage() string {
 	return `message :
-  Show a single formatted message.
+  Show a single formatted message.  By default an internal template
+ will be used, but you may specify the filename of a Golang text/template
+ file to use for rendering if you wish.
 `
 }
 
@@ -31,20 +51,23 @@ func (*messageCmd) Usage() string {
 // Flag setup
 //
 func (p *messageCmd) SetFlags(f *flag.FlagSet) {
+	f.StringVar(&p.template, "template", "", "Specify the path to a golang text/template file to use for message-rendering")
+	f.BoolVar(&p.dumpTemplate, "dump-template", false, "Dump the default template")
 }
 
-// Show the specified email
+// Show the specified email, with the appropriate template
 func (p *messageCmd) GetMessage(path string) (string, error) {
 
-	//
-	// We'll format the message with a template
-	//
-	tmpl := `To: {{.To}}
-From: {{.From}}
-Date: {{.Date}}
-Subject: {{.Subject}}
+	// Load the default template
+	tmpl := defaultTemplate
 
-{{.Body}}`
+	if p.template != "" {
+		content, err := ioutil.ReadFile(p.template)
+		if err != nil {
+			return "", err
+		}
+		tmpl = string(content)
+	}
 
 	// This is the structure we'll use to populate that
 	// template with.
@@ -84,6 +107,11 @@ Subject: {{.Subject}}
 
 // Entry-point.
 func (p *messageCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+
+	if p.dumpTemplate {
+		fmt.Println(defaultTemplate)
+		return subcommands.ExitSuccess
+	}
 
 	for _, path := range f.Args() {
 		out, err := p.GetMessage(path)
